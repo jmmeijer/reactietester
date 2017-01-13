e637a0b3e1101aa35dd3685fedb544e9bf157f21
Jesse Meijer <j.m.meijer@outlook.com>
2017-01-13 09:55:12 +0100

    Labels toevoegen

--- EindProject2/src/jesse/ScorePaneel.java
+++ EindProject2/src/jesse/ScorePaneel.java
@@ -1,12 +1,27 @@
 package jesse;
 
+import java.awt.*;
+
 import javax.swing.*;
 
 public class ScorePaneel extends JPanel {
+	private JLabel[] labels;
 
 	public ScorePaneel(ReactietestModel model){
 		
+		setLayout( new GridLayout(3,1) );
 		
+		labels = new JLabel[2];
+		Font font = new Font("SansSerif", Font.BOLD,12);
+		
+		//TODO: aanpassen
+		labels[0]= new JLabel("Score");
+		labels[1] = new JLabel("Fouten");
+		
+		for (JLabel label : labels){
+			label.setFont(font);
+			add(label);
+		}
 		
 	}
 	
