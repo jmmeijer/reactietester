f2f6f3383ce8e90c62daee2bcdce3b1cd787d4a5
Jesse Meijer <j.m.meijer@outlook.com>
2017-01-13 09:16:00 +0100

    ActionListener toegevoegd voor knoppen

--- EindProject2/src/jesse/BedieningsPaneel.java
+++ EindProject2/src/jesse/BedieningsPaneel.java
@@ -1,25 +1,91 @@
 package jesse;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class BedieningsPaneel extends JPanel {
 	
-	private JButton startKnop, balKnop, driehoekKnop, blokKnop, rechthoekKnop;
+	private JButton startKnop, circelKnop, driehoekKnop, vierkantKnop, rechthoekKnop;
 
 	public BedieningsPaneel(){
 		
+		
+		
+		
 		startKnop = new JButton("Start");
-		balKnop = new JButton("Start");
-		driehoekKnop = new JButton("Start");
-		blokKnop = new JButton("Start");
-		rechthoekKnop = new JButton("Start");
+		circelKnop = new JButton("Circel");
+		driehoekKnop = new JButton("Driehoek");
+		vierkantKnop = new JButton("Vierkant");
+		rechthoekKnop = new JButton("Rechthoek");
+		
+		startKnop.setActionCommand("start");
+		circelKnop.setActionCommand("circel");
+		driehoekKnop.setActionCommand("driehoek");
+		vierkantKnop.setActionCommand("vierkant");
+		rechthoekKnop.setActionCommand("rechthoek");
+		
+		ActionListener handler = new Handler();
+		
+		startKnop.addActionListener(handler);
+		circelKnop.addActionListener(handler);
+		driehoekKnop.addActionListener(handler);
+		vierkantKnop.addActionListener(handler);
+		rechthoekKnop.addActionListener(handler);
+		
+		circelKnop.setEnabled(false);
+		driehoekKnop.setEnabled(false);
+		vierkantKnop.setEnabled(false);
+		rechthoekKnop.setEnabled(false);
 		
 		add(startKnop);
-		add(balKnop);
+		add(circelKnop);
 		add(driehoekKnop);
-		add(blokKnop);
+		add(vierkantKnop);
 		add(rechthoekKnop);
 		
+	}
+	
+	public void start(){
+		circelKnop.setBackground(Color.RED);
+		driehoekKnop.setBackground(Color.GREEN);
+		vierkantKnop.setBackground(Color.YELLOW);
+		rechthoekKnop.setBackground(Color.BLUE);
+		rechthoekKnop.setForeground(Color.WHITE);
+		
+		startKnop.setEnabled(false);
+		circelKnop.setEnabled(true);
+		driehoekKnop.setEnabled(true);
+		vierkantKnop.setEnabled(true);
+		rechthoekKnop.setEnabled(true);
+	}
+	
+	//inwendige actionhandler klasse
+	class Handler implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			String command = e.getActionCommand();
+			
+			switch (command) {
+            case "start":  start();
+                     break;
+            case "circel":  
+                     break;
+            case "driehoek":  
+                     break;
+            case "vierkant":  
+                     break;
+            case "rechthoek":  
+                     break;
+            default:
+                     break;
+        }
+			
+		}
 	}
 	
 }
