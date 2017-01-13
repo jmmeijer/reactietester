bc76e0ff93be6f441b2adc6583f0e9d9d1c7a646
Jesse Meijer <j.m.meijer@outlook.com>
2017-01-13 09:17:43 +0100

    Dimensies uitgeschakeld voor een minder breed scorepaneel

--- EindProject2/src/jesse/Paneel.java
+++ EindProject2/src/jesse/Paneel.java
@@ -15,12 +15,15 @@ public class Paneel extends JPanel {
 		model = new ReactietestModel();
 		
 		scorePaneel = new ScorePaneel( model );
-		scorePaneel.setPreferredSize(new Dimension(800, 50));
+		//scorePaneel.setPreferredSize(new Dimension(800, 50));
 		
 		view = new ReactietestView( model );
 		view.setPreferredSize(new Dimension(100, 150));
 		
+		bedieningsPaneel = new BedieningsPaneel();
+		
 		add(view, BorderLayout.CENTER);
+		add(scorePaneel, BorderLayout.LINE_END);
 		add(bedieningsPaneel, BorderLayout.PAGE_END);
 		
 	}
