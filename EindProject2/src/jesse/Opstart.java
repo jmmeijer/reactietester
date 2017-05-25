2203f3af84c4927a715ded938922e35ef8896bb4
Jesse Meijer <j.m.meijer@outlook.com>
2017-05-25 14:23:17 +0200

    Added Welcome-dialog with explanation

--- EindProject2/src/jesse/Opstart.java
+++ EindProject2/src/jesse/Opstart.java
@@ -1,5 +1,7 @@
 package jesse;
 
+import java.awt.Font;
+
 import javax.swing.*;
 
 /**
@@ -11,13 +13,14 @@ import javax.swing.*;
  */
 
 @SuppressWarnings("serial")
-public class Opstart extends JFrame {
+public class Opstart {
 
 	/*
 	 * main methode voor de applicatie.
 	 */
 	public static void main( String args[] ) {
 		
+		Opstart opstart = new Opstart();
 		/*
 		 * Maak een nieuw JFrame aan.
 		 */
@@ -39,6 +42,39 @@ public class Opstart extends JFrame {
 		
 		frame.setVisible( true );
 			
+		opstart.showWelkom();
+	}
+	
+	public void showWelkom(){
+		JTextPane output = new JTextPane();
+        output.setEditable(false);
+        output.setContentType("text/html");
+        // Zorg ervoor dat de font wordt toegepast op html, zie: https://stackoverflow.com/questions/9335604/java-change-font-in-a-jtextpane-containing-html
+        output.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, true);
+        output.setFont(new Font("Arial", Font.PLAIN, 14));
+        JScrollPane scrollPane = new JScrollPane(output);
+        
+		String s = "<html>"
+				+ "<b>Uitleg Reactietest</b><br>"
+				+ "Reactietest om je reactiesnelheid mee te testen.<br><br>"
+				+ "<b>Speldoel</b><br>"
+				+ "Het doel van het spel is om zo snel mogelijk op de juiste knop te klikken bij het verschijnen van een vorm.<br><br>"
+				+ "<b>Spelverloop</b>"
+				+ "<ol>"
+				+ "<li>Op een willekeurig moment verschijnt een vorm</li>"
+				+ "<li>Klik op de bijbehorende knop</li>"
+				+ "<li>Jouw gemiddelde reactiesnelheid wordt berekend.</li>"
+				+ "</ol><br>"
+				+ "<b>Veel speelplezier!</b><br><br>"
+				+ "</html>";
+        output.setText(s);
+        // Zet de scrollbalk bovenaan
+        output.setCaretPosition(0);
+		
+		JOptionPane.showMessageDialog(null,
+				scrollPane,
+                "Welkom!",
+                JOptionPane.PLAIN_MESSAGE);
 	}
 	
 }
